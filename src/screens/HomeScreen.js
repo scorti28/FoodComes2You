9ee2056953db3539d74237558f384bcf7f411555
andreeascortan28 <olivera.scortan@student.upt.ro>
2023-11-27 23:10:03 +0200

    HomeScreen in progress

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,44 @@
 import HomeHeader from "../components/HomeHeader"
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {Icon} from 'react-native-elements';
+import {colors} from '../global/styles';
 
 export default function HomeScreen(){
+    const [delivery, setDelivery] = useState(true);
+
     return(
         <View style={styles.container}>
             <HomeHeader />
+
+            <View style= {{marginTop:10, flexDirection:'row', justifyContent:"space-evenly"}}>
+                <TouchableOpacity onPress={() => setDelivery(true)}>
+                    <View style = {{...styles.deliveryButton, backgroundColor:delivery?colors.buttons:colors.grey5}}>
+                        <Text style={styles.deliveryText}>Delivery</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => setDelivery(false)}>
+                    <View style = {{...styles.deliveryButton, backgroundColor:delivery?colors.grey5:colors.buttons}}>
+                        <Text style={styles.deliveryText}>Pick-Up</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
+        flex:1
+    },
+    deliveryButton:{
+        paddingHorizontal:20,
+        borderRadius:15,
+        paddingVertical:5
+    },
+    deliveryText:{
+        marginLeft:5,
+        fontSize:16
     }
 })
