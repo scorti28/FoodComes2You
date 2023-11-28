1dd09fa4bc72e1f6fdef7ac0d6dba571572401dd
andreeascortan28 <olivera.scortan@student.upt.ro>
2023-11-28 12:12:46 +0200

    HomeScreen in progress 2

---
+++ src/global/Data.js
@@ -0,0 +1,8 @@
+export const filterData = [
+         {name:"Fast Food", image: require("../images/FastFood.jpeg"), id:"0"},
+         {name:"Chinese", image: require("../images/Chinese.jpeg"), id:"1"},
+         {name:"Mexican", image: require("../images/Mexican.jpeg"), id:"2"},
+         {name:"Romanian", image: require("../images/RomanianFood.jpeg"), id:"3"},
+         {name:"Salads", image: require("../images/Salads.jpeg"), id:"4"},
+         {name:"Sea Food", image: require("../images/SeaFood.jpeg"), id:"5"}
+];
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,23 @@
 import HomeHeader from "../components/HomeHeader";
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, FlatList, Pressable, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { colors } from '../global/styles';
+import {filterData} from '../global/Data';
 
 export default function HomeScreen() {
     const [delivery, setDelivery] = useState(true);
+    const [indexCheck, setIndexCheck] = useState("0");
 
     return (
         <View style={styles.container}>
             <HomeHeader />
-            <ScrollView>
-                <View style={{ marginTop: 10, flexDirection: 'row', justifyContent: "space-evenly" }}>
+            <ScrollView
+                stickyHeaderIndices={[0]}
+                showsVerticalScrollIndicator = {true}
+            >
+            <View>
+                <View style={styles.deliveryCofiguration}>
                     <TouchableOpacity onPress={() => setDelivery(true)}>
                         <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.buttons : colors.grey5 }}>
                             <Text style={styles.deliveryText}>Delivery</Text>
@@ -24,6 +30,72 @@ export default function HomeScreen() {
                         </View>
                     </TouchableOpacity>
                 </View>
+            </View>
+            <View style = {styles.filterView}>
+                <View style={styles.lineComponent} >
+                  <View style = {styles.mapIcon}>
+                    <Icon 
+                        type = "material-community"
+                        name = "map-marker-multiple"
+                        color = {colors.grey1}
+                        size={26}
+                    />
+
+                    <Text style = {{marginLeft:5}}>19 Ioan Nistor</Text>
+                    </View> 
+
+                    <View style = {styles.nowIcon}>
+                    <Icon 
+                        type = "material-community"
+                        name = "web-clock"
+                        color = {colors.grey1}
+                        size={26}
+                    />
+
+                    <Text style = {{marginLeft:5}}>Now</Text>
+                    </View> 
+                </View>
+                <View>
+                    <Icon 
+                        type = "material-community"
+                        name = "tune-variant"
+                        color = {colors.grey1}
+                        size={26}
+                    />
+                </View>
+            </View>
+
+            <View style = {styles.categoriesTextView}>
+                <Text style = {styles.categoriesStyle}>Categories</Text>
+            </View>
+
+            <View>
+                <FlatList
+                    horizontal = {true}
+                    showsHorizontalScrollIndicator = {false}
+                    data = {filterData}
+                    keyExtractor = {(item) => item.id}
+                    extraData = {indexCheck}
+                    renderItem = {({item, index}) => (
+                        <Pressable onPress = {() => {setIndexCheck(item.id)}}>
+
+                            <View style = {indexCheck === item.id ? {...styles.smallCardSelected}:{...styles.smallCard}}>
+                                <Image 
+                                    style = {styles.imageCardStyle}
+                                    source = {item.image}
+                                />
+                                <View>
+                                    <Text style = {indexCheck === item.id ? {...styles.textCardSelectedStyle} : {...styles.textCardNotSelectedStyle}}>{item.name}</Text>
+                                </View>
+                            </View>
+                        </Pressable>
+                    )}
+                
+                />
+            </View>
+            <View style = {styles.categoriesTextView}>
+                <Text style = {styles.categoriesStyle}>Free Delivery Now</Text>
+            </View>
             </ScrollView>
         </View>
     );
@@ -41,5 +113,82 @@ const styles = StyleSheet.create({
     deliveryText: {
         marginLeft: 5,
         fontSize: 16
+    },
+    filterView:{
+        flexDirection:"row", 
+        alignItems:"center", 
+        justifyContent:"space-evenly", 
+        marginHorizontal:10, 
+        marginVertical:10
+    },
+    nowIcon:{
+        flexDirection:"row", 
+        alignItems:"center", 
+        marginLeft:20,
+        backgroundColor:colors.cardbackground,
+        borderRadius:15,
+        paddingHorizontal:5,
+        marginRight:20
+    },
+    mapIcon:{
+        flexDirection:"row", 
+        alignItems:"center", 
+        paddingLeft:20
+    },
+    lineComponent:{
+        flexDirection:"row", 
+        backgroundColor:colors.grey5, 
+        borderRadius:15, 
+        paddingVertical:3,
+        justifyContent:"space-between",
+        paddingHorizontal:10
+    },
+    deliveryCofiguration:{
+        marginTop: 10, 
+        flexDirection: 'row', 
+        justifyContent: "space-evenly"
+    },
+    categoriesStyle:{
+        color:colors.grey1,
+        fontSize:24,
+        fontWeight:"bold",
+        paddingLeft:20
+    },
+    categoriesTextView:{
+        backgroundColor:colors.grey5,
+        paddingVertical:3
+    },
+    smallCard:{
+        borderRadius:30,
+        backgroundColor:colors.grey5,
+        justifyContent:"center",
+        alignItems:"center",
+        padding:5,
+        width:80,
+        margin:10,
+        height:100
+    },
+    smallCardSelected:{
+        borderRadius:30,
+        backgroundColor:colors.buttons,
+        justifyContent:"center",
+        alignItems:"center",
+        padding:5,
+        width:80,
+        margin:10,
+        height:100
+    }, 
+    imageCardStyle:{
+        height:60,
+        width:60,
+        borderRadius:30
+    },
+    textCardSelectedStyle:{
+        fontWeight:"bold",
+        color: colors.cardbackground
+    },
+    textCardNotSelectedStyle:{
+        fontWeight:"bold",
+        color: colors.grey2
     }
 });
