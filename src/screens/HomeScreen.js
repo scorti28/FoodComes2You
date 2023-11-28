2df0c1a62c75a20f9bcacd3ae4e3eb94ac5105a5
andreeascortan28 <olivera.scortan@student.upt.ro>
2023-11-28 09:59:37 +0200

    HomeScreen corrected

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,44 +1,45 @@
-import HomeHeader from "../components/HomeHeader"
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {Icon} from 'react-native-elements';
-import {colors} from '../global/styles';
+import HomeHeader from "../components/HomeHeader";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { colors } from '../global/styles';
 
-export default function HomeScreen(){
+export default function HomeScreen() {
     const [delivery, setDelivery] = useState(true);
 
-    return(
+    return (
         <View style={styles.container}>
             <HomeHeader />
+            <ScrollView>
+                <View style={{ marginTop: 10, flexDirection: 'row', justifyContent: "space-evenly" }}>
+                    <TouchableOpacity onPress={() => setDelivery(true)}>
+                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.buttons : colors.grey5 }}>
+                            <Text style={styles.deliveryText}>Delivery</Text>
+                        </View>
+                    </TouchableOpacity>
 
-            <View style= {{marginTop:10, flexDirection:'row', justifyContent:"space-evenly"}}>
-                <TouchableOpacity onPress={() => setDelivery(true)}>
-                    <View style = {{...styles.deliveryButton, backgroundColor:delivery?colors.buttons:colors.grey5}}>
-                        <Text style={styles.deliveryText}>Delivery</Text>
-                    </View>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => setDelivery(false)}>
-                    <View style = {{...styles.deliveryButton, backgroundColor:delivery?colors.grey5:colors.buttons}}>
-                        <Text style={styles.deliveryText}>Pick-Up</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity onPress={() => setDelivery(false)}>
+                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.grey5 : colors.buttons }}>
+                            <Text style={styles.deliveryText}>Pick-Up</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
+    container: {
+        flex: 1
     },
-    deliveryButton:{
-        paddingHorizontal:20,
-        borderRadius:15,
-        paddingVertical:5
+    deliveryButton: {
+        paddingHorizontal: 20,
+        borderRadius: 15,
+        paddingVertical: 5
     },
-    deliveryText:{
-        marginLeft:5,
-        fontSize:16
+    deliveryText: {
+        marginLeft: 5,
+        fontSize: 16
     }
-})
+});
